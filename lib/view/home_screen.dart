fe203b4e758d8a95cbb902f4e5f0807036fad174
dachan <dachanisfull@gmail.com>
2024-04-01 18:14:26 +0900

    complete

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -9,11 +9,41 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('app bar'),
       ),
-      body: Center(
-        child: Text(
-          'Home Screen',
-          style: TextStyle(fontSize: 50),
-        ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.red,
+            width: 300,
+            height: 300,
+            child: LayoutBuilder(builder: (context, constraints) {
+              return Row(
+                children: [
+                  Container(
+                    color: Colors.black,
+                    width: constraints.maxWidth / 3,
+                    height: 100,
+                    child: Text(
+                      '${constraints.maxWidth}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.black,
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              );
+            }),
+          ),
+        ],
       ),
     );
   }
